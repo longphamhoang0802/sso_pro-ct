a5c7e3bf7d13a109fd60f247892ead5bba61d76d
longphamhoang0802 <phamhoanglong.k@gmail.com>
2023-03-22 00:51:21 +0700

    lewlew

---
+++ SSO project/Controllers/LoginController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SSO_project.Models;
+
+namespace SSO_project.Controllers
+{
+    public class LoginController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //GET
+        public ActionResult Login()
+        {
+            return View();
+        }
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(Account account)
+        {
+           if(ModelState.IsValid)
+           {
+                return View("Login");
+           }
+           return View("Index");
+            
+        }
+        
+    }
+}
